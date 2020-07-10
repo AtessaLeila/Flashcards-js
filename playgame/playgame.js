ae02aee6a4a31e8c17be9d7aef48b489fdc4c085
“Atessadai” <Atessa.dailami@gmail.com>
2020-07-10 14:56:15 -0400

    added timer

--- playgame/playgame.js
+++ playgame/playgame.js
@@ -273,4 +273,25 @@ window.onkeydown = function (event) {
 
 }
 
+var seconds = 60;
+var timer;
+function myFunction() {
+    if (seconds < 60) {
+        document.getElementById("timer").innerHTML = seconds;
+    }
+    if (seconds > 0) {
+        seconds--;
+    } else {
+        clearInterval(timer);
+        alert("Game Over");
+    }
+}
+function myTimer() {
+    if (!timer) {
+        timer = window.setInterval(function () {
+            myFunction();
+        }, 1000);
+    }
+}
 
+document.getElementById("timer").innerHTML = "1:00"; 
