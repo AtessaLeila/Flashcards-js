647540d6491b240d5ebfcd7b47a36d183152cf82
“Atessadai” <Atessa.dailami@gmail.com>
2020-08-15 14:07:18 -0400

    commit

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 function howToPlay() {
-<<<<<<< HEAD
-    location.replace("file:///atessaleila.github.io/Flashcards-js/index.html")
-=======
     location.href("/Flashcards-js/index.html")
->>>>>>> b806aa3740f3646f348394a67a851ea2cfbd6dc9
 }
 
 
 function deck() {
-<<<<<<< HEAD
-    location.replace("file:///atessaleila.github.io/Flashcards-js/deck/deck.html")
-}
-
-function playGame() {
-    location.replace("file:///atessaleila.github.io/Flashcards-js/playgame1/playgame1.html")
-=======
     location.href("/Flashcards-js/deck/deck.html")
 }
 
 function playGame() {
     location.href("/Flashcards-js/playgame1/playgame1.html")
->>>>>>> b806aa3740f3646f348394a67a851ea2cfbd6dc9
 }
